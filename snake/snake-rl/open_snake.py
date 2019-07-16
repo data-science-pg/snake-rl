21a4cf4a68aa0560082f9b593f6f91afe3a3b010
Olek <aleksander.trepala@boombit.com>
2019-07-16 19:47:23 +0200

    working function for left button pres simulation (temporary only if Snake windows is active on top) added terminating Snake process when program exists

--- snake/snake-rl/open_snake.py
+++ snake/snake-rl/open_snake.py
@@ -1,24 +1,23 @@
 import sys
 import os
 import win32gui
+import win32api
 import win32con
 import win32api
 import subprocess
 import time
+import pyautogui
 
 
 def run_snake():
     snake_exec_path = "../../Game/Snake.exe"
     app_instance = subprocess.Popen(snake_exec_path)
     hwndMain = win32gui.FindWindow(None,"Snake")
+    return app_instance, hwndMain
 
-def do_left():    #not working yet
-    hwndMain = win32gui.FindWindow(None,"Snake")
-    hwndCurrent = win32gui.GetActiveWindow()
-    win32gui.SetActiveWindow(hwndMain)
-    #virtual_key = win32api.MapVirtualKey(win32con.VK_LEFT, 0);
-    result = win32api.PostMessage(hwndMain, win32con.WM_KEYDOWN, win32con.VK_LEFT, 21692417)#magic value extracted from debug
-    print(result)
-    time.sleep(0.2)
-    win32api.PostMessage(hwndMain, win32con.WM_KEYUP, win32con.VK_LEFT, -1052049407)#magic value extracted from debug
-    #todo: check how to add keyboard input
+def do_left(process,handle):    #not working yet
+    #hwndCurrent = win32gui.GetActiveWindow()
+    #result = win32gui.SetActiveWindow(handle)
+    pyautogui.press('left')
+
+    #win32gui.SetActiveWindow(hwndCurrent)
--- snake/snake-rl/snake_rl.py
+++ snake/snake-rl/snake_rl.py
@@ -1,8 +1,15 @@
 from CV_Detection import *
 from open_snake import *
+import time
+
 def main():
 
-    run_snake()   #olek: run gui
+    process, handle = run_snake()   #olek: run gui
+    time.sleep(5)
+    do_left(process, handle)
+    time.sleep(1)
+    process.terminate()
+
     #ImageDetection = CVDetector()
 
 main()
