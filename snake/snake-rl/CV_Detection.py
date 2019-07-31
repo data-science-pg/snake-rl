d4488d69ec0c3eed6333032fa2bc54ccb518a8a1
Olek <aleksander.trepala@boombit.com>
2019-07-31 20:03:04 +0200

    changed swap_channels method to static

--- snake/snake-rl/CV_Detection.py
+++ snake/snake-rl/CV_Detection.py
@@ -14,14 +14,15 @@ class CVDetector:
             __printscreen_pil = ImageGrab.grab(app_handle.Rect())
             __printscreen_numpy = np.array(__printscreen_pil.getdata(),dtype='uint8').reshape((__printscreen_pil.size[1],__printscreen_pil.size[0],3))
             
-            self.swap_channels(__printscreen_numpy,0,2)
+            CVDetector.swap_channels(__printscreen_numpy,0,2)
             
             cv2.imshow("Snake Window",__printscreen_numpy)
             if cv2.waitKey(25) & 0xFF == ord('q'):
                 cv2.destroyAllWindows()
                 break
 
-    def swap_channels(self,img,first_idx,second_idx):
+    @staticmethod
+    def swap_channels(img,first_idx,second_idx):
         channel = img[:,:,first_idx].copy()
         img[:,:,first_idx] = img[:,:,second_idx]
         img[:,:,second_idx] = channel
