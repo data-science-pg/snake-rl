0fcca3a75ca0a355f34f52d56d0a6ed06375049b
Olek <aleksander.trepala@boombit.com>
2019-07-31 20:01:19 +0200

    fixed channels in displayed image

--- snake/snake-rl/CV_Detection.py
+++ snake/snake-rl/CV_Detection.py
@@ -12,11 +12,19 @@ class CVDetector:
 
         while(app_handle.IsOpen()):
             __printscreen_pil = ImageGrab.grab(app_handle.Rect())
-            __printscreen_numpy = np.array(__printscreen_pil.getdata(),dtype='uint8').reshape((__printscreen_pil.size[1],__printscreen_pil.size[0],3)) 
+            __printscreen_numpy = np.array(__printscreen_pil.getdata(),dtype='uint8').reshape((__printscreen_pil.size[1],__printscreen_pil.size[0],3))
+            
+            self.swap_channels(__printscreen_numpy,0,2)
+            
             cv2.imshow("Snake Window",__printscreen_numpy)
             if cv2.waitKey(25) & 0xFF == ord('q'):
                 cv2.destroyAllWindows()
                 break
+
+    def swap_channels(self,img,first_idx,second_idx):
+        channel = img[:,:,first_idx].copy()
+        img[:,:,first_idx] = img[:,:,second_idx]
+        img[:,:,second_idx] = channel
 
     def __init__(self):
         pass
